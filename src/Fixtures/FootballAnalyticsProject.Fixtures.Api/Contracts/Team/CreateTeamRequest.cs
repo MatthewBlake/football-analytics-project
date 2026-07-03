17d0ec1459a9872ac25f87f01fa48fc30c7b7d63
Matthew Blake <mzb1998@gmail.com>
2026-07-03 22:48:30 +0100

    Rename Teams folder to Team

---
+++ src/Fixtures/FootballAnalyticsProject.Fixtures.Api/Contracts/Team/CreateTeamRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FootballAnalyticsProject.Fixtures.Api.Contracts.Teams
+{
+    public class CreateTeamRequest
+    {
+        public required string Name { get; set; }
+
+        public required string Colour { get; set; }
+    }
+}
