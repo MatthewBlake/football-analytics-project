08c1a63cd77e44bf1ff3a64997e082abc1377914
Matthew Blake <mzb1998@gmail.com>
2026-08-06 21:38:04 +0100

    Add teams angular page

--- src/Web/footabll-analytics-web/src/app/app.routes.ts
+++ src/Web/footabll-analytics-web/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home'
+import { Teams } from './features/teams/teams'
 
 export const routes: Routes = [
   {
     path: '',
     component: Home
+  },
+  {
+    path: 'teams',
+    component: Teams
   }
 ];
