c757e5d6dbb0042d774a7b81aea8793ca30dfcb2
Matthew Blake <mzb1998@gmail.com>
2026-08-06 23:04:43 +0100

    Add fixtures angular

--- src/Web/footabll-analytics-web/src/app/app.routes.ts
+++ src/Web/footabll-analytics-web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home'
 import { Teams } from './features/teams/teams'
+import { Fixtures } from './features/fixtures/fixtures'
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'teams',
     component: Teams
+  },
+  {
+    path: 'fixtures',
+    component: Fixtures
   }
 ];
